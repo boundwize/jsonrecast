43315fcc20c420502d913507d9fc3dfbb24dfe74
Abdul Malik Ikhsan <samsonasik@gmail.com>
2026-08-14 19:58:19 +0700

    skip AddDoesNotPerformAssertionToNonAssertingTestRector

--- rector.php
+++ rector.php
@@ -3,24 +3,27 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\PHPUnit\PHPUnit60\Rector\ClassMethod\AddDoesNotPerformAssertionToNonAssertingTestRector;
 
 return RectorConfig::configure()
     ->withSkip([
         '**/Fixtures/**',
+        AddDoesNotPerformAssertionToNonAssertingTestRector::class,
     ])
     ->withPaths([
         __DIR__ . '/src',
         __DIR__ . '/tests',
     ])
+    ->withRootFiles()
     ->withPhpSets()
     ->withPreparedSets(
+        deadCode: true,
         codeQuality: true,
         codingStyle: true,
-        deadCode: true,
-        naming: true,
-        privatization: true,
         typeDeclarations: true,
         typeDeclarationDocblocks: true,
+        privatization: true,
+        naming: true,
         phpunitCodeQuality: true
     )
     ->withComposerBased(phpunit: true)
